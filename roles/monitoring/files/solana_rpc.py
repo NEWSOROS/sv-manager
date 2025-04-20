1f2fd3da7ca78d5ffb07481e3fd09877828e2546
NEWSOROS <mikhailo.robochyi@gmail.com>
2025-04-20 11:59:29 +0300

    Add files via upload

--- roles/monitoring/files/solana_rpc.py
+++ roles/monitoring/files/solana_rpc.py
@@ -125,14 +125,13 @@ def load_solana_validators(config: ValidatorConfig):
     cmd = f'solana validators -ul --output json-compact'
     data = execute_cmd_str(config, cmd, convert_to_json=True)
     
-    if (data is not None) and ('validators' in data):
+    if data is not None and 'validators' in data:
         validators = data['validators']
-        sorted_validators = sorted(validators, key=lambda x: x['credits'], reverse=True)
+        sorted_validators = sorted(validators, key=lambda x: x.get('epochCredits', 0), reverse=True)
         for i, validator in enumerate(sorted_validators, start=1):
             validator['place'] = i
         return sorted_validators
-    else:
-        return None
+    return None
 
 
 def load_stakes(config: ValidatorConfig, vote_account):
